170d3463abeebfc7a285d5c3d0ee76c03f324e55
Andrew Stahl <andrewstahl96@gmail.com>
2022-09-05 17:51:48 -0400

    Files working with SQL and ruby

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 5, "M", "Brown", "Nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 7, "M", "Black", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 1, "F", "Brown", "Grumpy", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "Brown", "Nice", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 25, "F", "White", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 35, "M", "Black", "Nice", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 75, "F", "Brown", "Mean", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 5, "F", "Black", "Mean", 0);
